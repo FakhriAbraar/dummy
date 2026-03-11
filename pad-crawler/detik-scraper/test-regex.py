37a647cf929654c7ba43338f9b8575cb370ce258
algof <algofkz@gmail.com>
2026-03-11 15:22:53 +0700

    remove dummy file in detik-scraper

--- pad-crawler/detik-scraper/test-regex.py
+++
@@ -1,8 +0,0 @@
-import re
-
-# text = "Nomor saya 12345"
-text = "https://news.detik.com/berita/d-8394275/5-fakta-pembunuh-ermanto-usman-di-bekasi-ditangkap-polisi"
-
-hasil = re.search(r"\d+", text)
-
-print(hasil.group())
